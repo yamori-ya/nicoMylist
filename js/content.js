af957c90d16a444fc9148b13c1e60e15ef48f581
y_kahou <yamorikahou@gmail.com>
2021-08-25 16:20:09 +0900

    画面クリックの再生停止機能削除

--- js/content.js
+++ js/content.js
@@ -1,71 +1,4 @@
 
-$.fn.tap = function() {
-	return $(this).on({
-		touchstart: function(e) {
-			$(this).data('tap', true)
-			var to = setTimeout(() => {
-				$(this).data('tap', false)
-				$(this).trigger('subfocus')
-			}, 200)
-			$(this).data('tap_to', to)
-		},
-		touchmove: function(e) {
-			if ($(this).data('tap')) {
-				$(this).trigger('subfocus')
-				$(this).data('tap', false)
-				clearTimeout($(this).data('tap_to'))
-			}
-		},
-		touchend: function(e) {
-			if ($(this).data('tap')) {
-				$(this).data('tap', false)
-				clearTimeout($(this).data('tap_to'))
-				e.type = 'temp'
-				$(this).trigger(e)
-				e.preventDefault()
-			}
-		},
-		temp: function(e) {
-			e.touches = e.changedTouches
-			e.changedTouches = void(0)
-			if ($(this).data('dbltap')) {
-				$(this).data('dbltap', false)
-				clearTimeout($(this).data('dbltap_to'))
-				e.type = 'doubletap'
-				$(this).trigger(e)
-			} else {
-				$(this).data('dbltap', true)
-				var to = setTimeout(() => {
-					$(this).data('dbltap', false)
-					e.type = 'tap'
-					$(this).trigger(e)
-				}, 200)
-				$(this).data('dbltap_to', to)
-			}
-		}
-	})
-}
-
-const OVER_LAYER = `
-<div id="over-layer">
-	<div id="play" class="controll-button">
-		<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 13.229 13.229">
-			<path d="M13.23 6.615a6.615 6.615 0 0 1-6.615 6.614A6.615 6.615 0 0 1 0 6.615 6.615 6.615 0 0 1 6.615 0a6.615 6.615 0 0 1 6.614 6.615z"/>
-			<path d="M10.054 6.615l-5.16 2.978V3.636z" fill="#fff"/>
-		</svg>
-	</div>
-	<div id="pause" class="controll-button">
-		<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 13.229 13.229">
-			<g transform="translate(0 -283.77)">
-				<circle cx="6.615" cy="290.385" r="6.615"/>
-				<path fill="#fff" d="M3.969 287.21h1.852v6.35H3.969zM7.408 287.21H9.26v6.35H7.408z"/>
-			</g>
-		</svg>
-	</div>
-</div>`
-
-
-
 // 動画情報
 var video_info;
 if (document.domain == "www.nicovideo.jp") {
@@ -73,24 +6,43 @@ if (document.domain == "www.nicovideo.jp") {
 }
 else if (document.domain == "www.youtube.com") {
 	
+	function getDomText(selector) {
+		return new Promise((resolve, reject) => {
+			let cnt = 0;
+			let iv = setInterval(() => {
+				if (++cnt > 10) {
+					clearInterval(iv)
+					resolve('')
+				}
+				let dom = document.querySelector(selector)
+				if (dom && dom.textContent.trim()) {
+					clearInterval(iv)
+					resolve(dom.textContent)
+				}
+			}, 500)
+		})
+	}
+	Promise.all([
+		getDomText('h1.title'),
+		getDomText('.super-title'),
+		getDomText('.ytp-time-duration')
+	]).then(val => {
+		let id = location.href.match(/watch\?v=(.+)/)[1], now = new Date()
+		videoInfo = [
+			"def", "index", location.href, val[0],
+			`http://img.youtube.com/vi/${id}/hqdefault.jpg`,
+			val[1], val[2], "",
+			 `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`,
+		];
+		// popへ動画情報送信　のためにbackgroundへ送信
+		sendMessage({id:"getVideoInfo", data:videoInfo})
+	})
 }
 
-// popへ動画情報送信
-// chrome.runtime.onMessage.addListener(
-// 	function(request, sender, sendResponse) {
-// 		if (request.id == "getVideoInfo") {
-// 			sendResponse({data: video_info});
-// 		}
-// 		return true;
-// 	}
-// );
 
 
-const PROP = [
-	"player_tab_id",
-	"nico_full_screen",
-	"nico_setting",
-];
+
+
 
 // videoを正常に取得できるまで500ms間隔で取得
 function getVideo() {
@@ -108,13 +60,14 @@ function getVideo() {
 
 
 Promise.all([
-	getLocalStorage(PROP),
+	getLocalStorage(["player_tab_id", "nico_full_screen"]),
+	getSyncStorage("nico_setting"),
 	sendMessage({id:"get_current_tab"}),
 ]).then((values) => {
 	
 	var storage = values[0];
-	var nico_setting = storage.nico_setting;
-	var currentTabId = values[1].tab.id;
+	var nico_setting = values[1];
+	var currentTabId = values[2].tab.id;
 	
 	if (document.domain == "www.nicovideo.jp") {
 		
@@ -137,11 +90,10 @@ Promise.all([
 		}
 		// 通常再生時
 		else {
-			if (nico_setting.just_scroll) justScroll();
+			if (nico_setting.scroll) justScroll();
 		}
 		
 		getVideo().then(video => {
-			$('.PlayerContainer').focus();
 			$(video).on('ended', function() {
 				console.log("video ended");
 				// 現在動画がフルスクリーンだったら次もフルスクリーンにするために状態を保存
@@ -149,44 +101,6 @@ Promise.all([
 				// マイリストタブへ動画終了を通知
 				sendMessage({id:"video_ended"});
 			});
-			
-			// タップ操作処理
-			$('.InView.VideoContainer').tap()
-			.on('tap click', function(e) {
-				$('.PlayerPlayButton, .PlayerPauseButton').click()
-			})
-			.on('doubletap', function(e) {
-				var lr = ($(this).width() / 2 < e.touches[0].pageX - $(this).offset().left)
-				$(lr ? '.PlayerSeekForwardButton' : '.PlayerSeekBackwardButton').click()
-			})
-			.on('subfocus', function(e) {
-				if ($('.is-fullscreen').length && !$('.is-fixedFullscreenController').length) {
-					var container = $(this).parent()
-					container.addClass('is-mouseMoving')
-					clearTimeout($(this).data('focus_to'))
-					var to = setTimeout(() => {
-						container.removeClass('is-mouseMoving')
-					}, 2000)
-					$(this).data('focus_to', to)
-				}
-			})
-			// 初回再生で再生停止機能、アイコン追加
-			$(video).on('play', function() {
-				$('.InView.VideoContainer').append(OVER_LAYER)
-				$(video).off('play')
-			})
-			// アイコン処理
-			var pause_play = function() {
-				if (video.paused) {
-					$('#pause').css('visibility', 'hidden' ).removeClass('click')
-					$('#play' ).css('visibility', 'visible').addClass('click')
-				} else {
-					$('#pause').css('visibility', 'visible').addClass('click')
-					$('#play' ).css('visibility', 'hidden' ).removeClass('click')
-				}
-			}
-			$('.ControllerContainer-inner').on('click', '.PlayerPlayButton, .PlayerPauseButton', pause_play)
-			$('.PlayerContainer').on('keydown', e => { if (e.keyCode == 32) pause_play() })
 		});
 		
 	}
