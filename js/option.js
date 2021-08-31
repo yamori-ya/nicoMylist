7186514bfe15a0a966ec686e77a42b9f193e23d8
y_kahou <yamorikahou@gmail.com>
2021-08-31 13:51:39 +0900

    オプションリファクタ、読込中作成

--- js/option.js
+++ js/option.js
@@ -2,50 +2,49 @@ var listSheetId
 var infoSheetId
 var changeBookId = false
 
-function addBook() {
+async function addBook() {
 	if (!window.confirm("スプレッドシートを新規作成します"))
 		return false
-		
-	var api = new SheetApi('')
-	api.CreateBook()
-	.then(ids => {
-		$('#bookId').val(ids.book);
-		listSheetId = ids.list;
-		infoSheetId = ids.info;
-		return Promise.all([
-			api.AppendData('info', ['def']),
-			setSyncStorage({ids: ids, reload: true})
-		])
-	})
-	.then(() => {
-		alert('スプレッドシートを作成しました！')
-	})
+	
+	$('#load-layer').show()
+	let api = new SheetApi('')
+	let ids = await api.CreateBook()
+	$('#bookId').val(ids.book);
+	listSheetId = ids.list;
+	infoSheetId = ids.info;
+	await api.AppendData('info', ['def']),
+	await setSyncStorage({ids: ids, reload: true})
+	alert('スプレッドシートを作成しました！')
+	$('#load-layer').hide()
 }
-function getSheetId(bookId) {
-	var api = new SheetApi(bookId)
-	api.GetBookInfo()
-	.then(obj => {
-		if (obj.error) { // シートへのアクセス失敗
-			goOption('error' + obj.error.code)
-			return;
-		}
-		for (var one of obj.sheets) {
-			switch(one.properties.title) {
-			case "list": listSheetId = one.properties.sheetId; break;
-			case "info": infoSheetId = one.properties.sheetId; break; }
-		}
-		if (listSheetId == null || infoSheetId == null) {
-			goOption('error' + obj.error.code)
-			return;
-		}
-		setSyncStorage({
-			ids : {
-				book: bookId,
-				list: listSheetId,
-				info: infoSheetId
-			},
-			reload: true
-		});
+async function getSheetId(bookId) {
+	if (!bookId) {
+		goOption('error' + 404)
+		return;
+	}
+	let api = new SheetApi(bookId)
+	let res = await api.GetBookInfo()
+	
+	if (res.error) { // シートへのアクセス失敗
+		goOption('error' + res.error.code)
+		return;
+	}
+	for (let one of res.sheets) {
+		switch(one.properties.title) {
+		case "list": listSheetId = one.properties.sheetId; break;
+		case "info": infoSheetId = one.properties.sheetId; break; }
+	}
+	if (listSheetId == null || infoSheetId == null) {
+		goOption('error' + res.error.code) // シートID取得できず
+		return;
+	}
+	return setSyncStorage({
+		ids : {
+			book: bookId,
+			list: listSheetId,
+			info: infoSheetId
+		},
+		reload: true
 	});
 }
 
@@ -65,12 +64,12 @@ $(function() {
 		}
 	})
 	
-	var params = getUrlParams();
+	let params = getUrlParams();
 	if (Object.keys(params).length != 0) {
 		if (params["status"] == "empty") {
-			$('#error_str').html("BookIDが設定されていません");
+			$('#error_str').text("BookIDが設定されていません");
 		} else if (params["status"].match(/^error(\d+)/)) {
-			$('#error_str').html("BookIDが不正です: " + params["status"]);
+			$('#error_str').text("BookIDが不正です: " + params["status"]);
 		}
 	}
 	
@@ -85,16 +84,20 @@ $(function() {
 	
 	$('#create').on('click', addBook)
 	
-	$('#save').on('click', function() {
-		if (changeBookId) {
-			getSheetId($('#bookId').val())
-		}
-		setSyncStorage({
+	$('#save').on('click', async function() {
+		
+		$('#load-layer').show()
+		await setSyncStorage({
 			nico_setting : {
 				scroll:     $('#nico-scroll').prop('checked'),
 				click2play: $('#nico-click2play').prop('checked')
 			}
 		})
+		if (changeBookId) {
+			await getSheetId($('#bookId').val())
+		}
 		$('#save').attr('disabled','')
+		$('#error_str').text('')
+		$('#load-layer').hide()
 	});
 });
